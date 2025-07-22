beff2db40fc041da0179684b6b88b966e1ccf648
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-07-22 21:35:35 +0200

    try on more runners + improve test

--- src/test/scala/SocketTests.scala
+++ src/test/scala/SocketTests.scala
@@ -1,38 +1,34 @@
 package it.unibo.scafi.runtime.network.sockets
 
-import org.scalatest.concurrent.Eventually
-import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.{ Eventually, ScalaFutures }
 import org.scalatest.concurrent.ScalaFutures.convertScalaFuture
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
-import org.scalatest.time.Seconds
-import org.scalatest.time.Span
+import org.scalatest.time.{ Seconds, Span }
 
 import java.net.ServerSocket
-import java.util.concurrent.ForkJoinPool
+import java.util.concurrent.{ CountDownLatch, ForkJoinPool, TimeUnit }
 import scala.concurrent.*
 
 class SocketTests extends AnyFlatSpec with should.Matchers with ScalaFutures with Eventually:
 
   given ExecutionContext = ExecutionContext.fromExecutor(ForkJoinPool())
 
-  override given patienceConfig: PatienceConfig = PatienceConfig(timeout = scaled(Span(5, Seconds)))
+  override given patienceConfig: PatienceConfig = PatienceConfig(timeout = scaled(Span(10, Seconds)))
+
+  private val FreePort = 0
 
   "when blocked on accept and the server is closed" should "throw an exception" in:
     for i <- 0 until 10 do
-      println("Run " + i)
-      val freePort = 0
-      val startedPromise = Promise[Unit]()
-      val serverSocket = new ServerSocket(freePort)
-      println(">>>>> Starting server socket <<<<<")
+      val acceptStarted = new CountDownLatch(1)
+      val serverSocket = new ServerSocket(FreePort)
       val serverFuture = Future:
-        println(">>>>> Accepting connection <<<<<")
-        startedPromise.success(())
+        acceptStarted.countDown()
         serverSocket.accept()
-        println("<<<< Effectively closed <<<<")
-      startedPromise.future.futureValue
-      Thread.sleep(1_000)
-      println(">>>>> Closing server socket <<<<<")
+      val started = acceptStarted.await(5, TimeUnit.SECONDS) // wait until accept is about to be called
+      if (!started) then fail("Server socket accept did not start after 5 seconds!")
+      Thread.sleep(1_000) // give some time for the accept to block
+      serverFuture.isCompleted shouldBe false
       serverSocket.close()
       whenReady(serverFuture.failed):
         _ shouldBe a[java.net.SocketException]
